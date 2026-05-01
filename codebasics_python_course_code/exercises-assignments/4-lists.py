45dca04883774ab4dd599f4a1052a3eb6c4ea0c6
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-05-01 16:50:46 +0530

    🚀

--- codebasics_python_course_code/exercises-assignments/4-lists.py
+++ codebasics_python_course_code/exercises-assignments/4-lists.py
@@ -13,6 +13,10 @@ items[-1]
 items[0:2]
 items[3:]
 
+# Clear the list, remove all the elements.
+# items.clear()
+# print(items)
+
 items.append('bread')
 print(items)
 
@@ -81,3 +85,63 @@ office_supplies = ["pen", "paper", "stapler"]
 kitchen_supplies = ["fork", "knife", "spoon"]
 combined_list =  kitchen_supplies + office_supplies
 print(combined_list[2: 4])
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+numbers[-4:]
+numbers[10] = 11 # index out of range
+numbers.append(11)
+numbers.insert(10, 11)
+print(numbers)
+
+
+x = [1, 2, 3]
+
+y = list(x)
+# OR
+# y = x[:]
+
+print(x)
+print(y)
+
+x[1] = 99
+
+print(x)
+print(y)
+
+x = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+y = list(x)
+
+print(x)
+print(y)
+
+x[1][1] = 50
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+
+del numbers[4:6]
+print(numbers)
+
+
+areas_ext = [
+    "hallway", 11.25,
+    "kitchen", 18.0,
+    "living room", 20.0,
+    "bedroom", 10.75,
+    "bathroom", 10.50,
+    "poolhouse", 24.5,
+    "garage", 15.45
+]
+print(areas_ext)
+
+del areas_ext[-4]
+del areas_ext[-3]
+
+print(areas_ext)
